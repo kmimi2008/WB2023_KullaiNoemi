23a1dada01ae342d82c2544be9df135292825936
kmimi2008 <kullai.noemi@diak.szbi-pg.hu>
2026-03-09 08:45:03 +0100

    03.09. órai munka

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _0305
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] szamok = new int[10, 10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    szamok[i, j] = rnd.Next(1, 100);
+                }
+            }
+
+            Console.WriteLine("Eredeti számok:");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(szamok[i, j].ToString().PadLeft(3) + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+           
+            for (int k = 1; k <= 4; k++)
+            {
+                int[,] elforgatott = new int[10, 10];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        elforgatott[j, 9 - i] = szamok[i, j];
+                    }
+                }
+
+                szamok = elforgatott;
+
+                
+
+                Console.WriteLine($"{k}. elforgatás:");
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.Write(szamok[i, j].ToString().PadLeft(3) + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
